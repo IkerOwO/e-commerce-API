42377c25b6975b498713985d81613b354963adb8
unknown <csiker12@gmail.com>
2026-08-20 05:16:49 +0200

    Query de CategoryRepository corregida

--- src/main/java/com/iker/ecommerce_api/repositories/CategoryRepository.java
+++ src/main/java/com/iker/ecommerce_api/repositories/CategoryRepository.java
@@ -7,7 +7,7 @@ import com.iker.ecommerce_api.entities.Category;
 import com.iker.ecommerce_api.entities.Product;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("SELECT p FROM Product p JOIN p.categories c WHERE c.name = ?1categoryName")
+    @Query("SELECT p FROM Product p JOIN p.categories c WHERE c.name = ?1")
     List<Product> getProductsFromCategoryName(String categoryName);
     boolean existsByName(String name);
 }
--- src/main/java/com/iker/ecommerce_api/services/CategoryService.java
+++ src/main/java/com/iker/ecommerce_api/services/CategoryService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.iker.ecommerce_api.dtos.Category.RegisterCategoryRequest;
 import com.iker.ecommerce_api.entities.Category;
 import com.iker.ecommerce_api.exceptions.Category.CategoryAlreadyExists;
